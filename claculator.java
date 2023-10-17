a622a20c68634c54d2021b0a5d9feda7fca527af
webashishvishwakarma <136798090+webashishvishwakarma@users.noreply.github.com>
2023-10-17 22:40:22 +0530

    Add files via upload

---
+++ claculator.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class claculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("enter the operator");
+        char operator =sc.next().charAt(0);
+        
+        switch(operator){
+           case '+': System.out.println("sum:"+(a+b));
+           break;
+       
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+    
+}
