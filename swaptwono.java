c4c2454c0ac7ad81e3dd530965342c8b5448f9e3
webashishvishwakarma <136798090+webashishvishwakarma@users.noreply.github.com>
2023-10-18 19:20:21 +0530

    Add files via upload

---
+++ swaptwono.java
@@ -0,0 +1,15 @@
+public class swaptwono {
+    public static void main(String[] args) {
+        
+        int a=10;
+        int b=20;
+        int t;
+         t = a;
+         a=b;
+         b=t;
+         
+         System.out.println("no of a"    + a);
+         System.out.println("b"+b);
+    }
+    
+}
