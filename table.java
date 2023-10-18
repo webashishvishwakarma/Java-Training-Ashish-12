e02a3b39c198edc271f5e9dfef2a8d9efb011960
webashishvishwakarma <136798090+webashishvishwakarma@users.noreply.github.com>
2023-10-18 19:19:56 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class table {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n = sc. nextInt();
+
+        for(int i=2; i<11; i++)
+        {
+            System.out.println(n*i);
+        }
+    }
+    
+}
